57ba7646584c2d2888c6b86483dbad73126fe350
Eduardo Lopes Ferreira <eduardoferreira@wine.com.br>
2021-01-04 16:34:17 -0300

    Refactoring action type for a new one

--- src/store/modules/products/reducers.ts
+++ src/store/modules/products/reducers.ts
@@ -1,43 +1,43 @@
-
-import { IErrorHandler, IStore, ActionsRequestTypes } from './types';
+import { IErrorHandler, IStore, ActionsRequest } from './types';
 
 const initialState: IStore = {
   products: [],
   errorHandler: {} as IErrorHandler,
 };
 
-const productReducer = (state = initialState, action: ActionsRequestTypes): IStore => {
+const productReducer = (state = initialState, action: ActionsRequest ): IStore => {
   switch(action.type) {
     case 'ADD_PRODUCT_REQUEST_SUCCEEDED':
     return {
       ...state,
-      products: [...state.products, action.product],
-    }
+      products: [...state.products, action.payload.product],
+    };
     case 'INCREMENT_PRODUCT_REQUEST_SUCCEEDED':
-    return {
+      return {
       ...state,
       products: state.products.map(product =>
-        product.id === action.product.id ? { ...action.product, quantity: action.product.quantity } : product,
+        product.id === action.payload.updatedProduct.id ? {
+          ...action.payload.updatedProduct,
+          quantity: action.payload.updatedProduct.quantity
+        } : product,
       ),
     }
     case 'DELETE_PRODUCT_REQUEST_SUCCEEDED':
     return {
       ...state,
-      products: state.products.filter(product => product.id !== action.product.id),
+      products: state.products.filter(product => product.id !== action.payload.product.id),
     }
     case 'GET_API_PRODUCTS_REQUEST_SUCCEEDED':
-    const { products } = action;
-    console.log(products);
-    return {
+      return {
       ...state,
-      products: products,
+      products: action.payload.products,
     }
     case 'REQUEST_FAILURE':
-    return {
+      return {
       ...state,
       errorHandler: {
-        content: action.error.content,
-        isErrored: action.error.isErrored,
+        content: action.payload.content,
+        isErrored: action.payload.isErrored,
       }
     }
     default: return state;
