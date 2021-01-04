1fc156e1f1b701d433d50d20a511abf0ee780845
Eduardo Lopes Ferreira <eduardoferreira@wine.com.br>
2021-01-04 16:27:40 -0300

    simplify succeeded and request types

--- src/store/modules/products/types.ts
+++ src/store/modules/products/types.ts
@@ -1,14 +1,9 @@
-import {
-  ADD_PRODUCT_REQUEST,
-  ADD_PRODUCT_REQUEST_SUCCEEDED,
-  DELETE_PRODUCT_REQUEST,
+import { 
   DELETE_PRODUCT_REQUEST_SUCCEEDED,
-  GET_API_PRODUCTS_REQUEST,
-  GET_API_PRODUCTS_REQUEST_SUCCEEDED,
-  INCREMENT_PRODUCT_REQUEST,
   INCREMENT_PRODUCT_REQUEST_SUCCEEDED,
-  REQUEST_FAILURE
-} from '../../../constants/types.constants';
+  ADD_PRODUCT_REQUEST_SUCCEEDED,
+} from "../../../constants/types.constants";
+
 export interface IProduct {
   id: number;
   name: string;
@@ -29,57 +24,29 @@ export interface IStore {
   products: IProduct[];
   errorHandler: IErrorHandler;
 };
-
-export interface IAddProductRequestAction {
-  type: typeof ADD_PRODUCT_REQUEST;
-  product: IProduct;
+export interface ActionsRequest {
+  type: string;
+  payload: any;
 }
 
-export interface IAddProductRequestSucceededAction {
-  type: typeof ADD_PRODUCT_REQUEST_SUCCEEDED;
-  product: IProduct;
-}
-
-export interface IGetAPIProductsRequestAction {
-  type: typeof GET_API_PRODUCTS_REQUEST;
-}
-
-export interface IGetAPIProductsRequestSucceededAction {
-  type: typeof GET_API_PRODUCTS_REQUEST_SUCCEEDED;
-  products: IProduct[];
-}
-
-
-export interface IIncrementProductRequestAction {
-  type: typeof INCREMENT_PRODUCT_REQUEST;
-  incrementedProduct: IIncrementedProduct;
-}
-export interface IIncrementProductRequestSucceededAction {
-  type: typeof INCREMENT_PRODUCT_REQUEST_SUCCEEDED;
-  product: IProduct;
-}
-
-export interface IDeleteProductRequestAction {
-  type: typeof DELETE_PRODUCT_REQUEST;
-  product: IProduct;
-}
-
-export interface IDeleteProductRequestSucceededAction {
+export interface IDeleteProductRequest {
   type: typeof DELETE_PRODUCT_REQUEST_SUCCEEDED;
-  product: IProduct;
+  payload: {
+    product: IProduct;
+  };
 }
 
-export type RequestFailureType = {
-  type: typeof REQUEST_FAILURE,
-  error: IErrorHandler,
+export interface IIncrementedProductRequest {
+  type: typeof INCREMENT_PRODUCT_REQUEST_SUCCEEDED;
+  payload: {
+    quantity: number;
+    product: IProduct;
+  };
 }
 
-export type ActionsRequestTypes = SucceededRequestActions | RequestFailureType;
-
-export type RequestActions = IDeleteProductRequestAction | IGetAPIProductsRequestAction | IAddProductRequestAction | IIncrementProductRequestAction;
-export type SucceededRequestActions = IAddProductRequestSucceededAction | IDeleteProductRequestSucceededAction | IGetAPIProductsRequestSucceededAction | IIncrementProductRequestSucceededAction | IAddProductRequestSucceededAction;
-
-export type DeleteProductActionsRequestsTypes = IDeleteProductRequestAction | IDeleteProductRequestSucceededAction;
-export type ActionsAPIRequestTypes = IGetAPIProductsRequestAction | IGetAPIProductsRequestSucceededAction;
-export type AddProductActionsRequestTypes = IAddProductRequestAction | IAddProductRequestSucceededAction;
-export type IncrementProductActionsRequestTypes = IIncrementProductRequestAction | IIncrementProductRequestSucceededAction;
+export interface IAddProductRequest {
+  type: typeof ADD_PRODUCT_REQUEST_SUCCEEDED;
+  payload: {
+    product: IProduct;
+  };
+}
